044d2ed2dbb6b1459cf9697b44e638a5ffca67be
Andrea Quinto <107708093+Andre1411@users.noreply.github.com>
2023-11-20 21:16:32 +0100

    Update bin_smoother.m

--- bin_smoother.m
+++ bin_smoother.m
@@ -1,30 +1,30 @@
-function [u_hat,res] = bin_smoother(ys,ts,tv,nbin)
+function [u_hat, res] = bin_smoother(ys, ts, tv, nbin)
 
-% riceve in ingersso il vettore dei tempi (ts) e delle misure (ys) del segnale,
-% la virtual grid (tv) ed il numero di bin (nbin), restituisce il valore dello
-% smoother sulla vg (u_hat) e i residui assoluti sulla sampling grid (res)
+% receives as input the time vector (ts) and signal measurements vector (ys),
+% the virtual grid (tv), and the number of bins (nbin). Returns the smoother value
+% on the virtual grid (u_hat) and the absolute residuals on the sampling grid (res)
 
-nv = length(tv) ;               % calcolo del numero di camioni della vg
-Tv = tv(2)-tv(1) ;              % calcollo del passo della vg
-u_hat = zeros(length(tv),1) ;   % inizializzazione smoother
-t_end = tv(end) ;
-dt = round(t_end/nbin) ;        % calcolo dell'ampiezza temporale del bin
-i = 1 ;
+nv = length(tv);               % calculation of the number of samples on the virtual grid
+Tv = tv(2) - tv(1);             % calculation of the virtual grid step
+u_hat = zeros(length(tv), 1);   % smoother initialization
+t_end = tv(end);
+dt = round(t_end/nbin);         % calculation of the temporal bin width
+i = 1;
 
-% ciclo di costruzione del bin
+% loop for constructing the bin
 while dt*i <= t_end
-    u_hat(tv<=dt*i & tv>dt*i-dt) = mean(ys(ts<=dt*i & ts>dt*i-dt)) ;
-    i = i+1 ;
+    u_hat(tv <= dt*i & tv > dt*i - dt) = mean(ys(ts <= dt*i & ts > dt*i - dt));
+    i = i + 1;
 end
 
-% calcolo dgli istanti sulla vg corrispondenti a quelli sulla sampling grid
-idx = zeros(nv,1) ;
+% calculation of instants on the virtual grid corresponding to those on the sampling grid
+idx = zeros(nv, 1);
 for i = 1:length(ts)
-    k = ts(i)/Tv ;
-    idx(k) = 1 ;
+    k = ts(i)/Tv;
+    idx(k) = 1;
 end
 
-idx = logical(idx) ;
-res = u_hat(idx)-ys ;           % calcolo dei residui assoluti
+idx = logical(idx);
+res = u_hat(idx) - ys;           % calculation of absolute residuals
 
 end
